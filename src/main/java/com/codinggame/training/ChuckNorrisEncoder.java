2fb7637bc0761dcf9ead72a1cbccf59509670b55
y0shi999 <inaflas.thami@gmail.com>
2019-04-22 15:23:16 +0200

    Init project Chuck Norris

---
+++ src/main/java/com/codinggame/training/ChuckNorrisEncoder.java
@@ -0,0 +1,52 @@
+package com.codinggame.training;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class ChuckNorrisEncoder
+{
+    static String encode(final String text) {
+        if(text == null || text.isEmpty())
+            return text;
+        else {
+            final String encodedText;
+            switch (text) {
+                case "A":
+                    encodedText = "0 0 00 00000 0 0";
+                    break;
+                case "B":
+                    encodedText = "0 0 00 0000 0 0 00 0";
+                    break;
+                case "C":
+                    encodedText = "0 0 00 0000 0 00";
+                    break;
+                default:
+                    encodedText = text;
+            }
+            return encodedText;
+        }
+    }
+
+    static String charEncode(final char character){
+        final StringBuilder representationBuilder = new StringBuilder();
+        final String binaryRepresentation = Integer.toBinaryString(character);
+        final List<String> parts = new ArrayList<>();
+        System.out.println(binaryRepresentation);
+        char previousChar = 0;
+        for (int i = 0; i < binaryRepresentation.length(); i++) {
+            if (i != 0 && previousChar == binaryRepresentation.charAt(i)) {
+                representationBuilder.delete(0, representationBuilder.length());
+                representationBuilder.append("0");
+            } else {
+                representationBuilder.delete(0, representationBuilder.length());
+                representationBuilder.append("0");
+                if ('0' == binaryRepresentation.charAt(i))
+                    representationBuilder.append("0");
+                representationBuilder.append(" ").append("0");
+                previousChar = binaryRepresentation.charAt(i);
+            }
+            parts.add(representationBuilder.toString());
+        }
+        return parts.stream().reduce((s, t) -> s + " " + t).orElse("");
+    }
+}
---
+++ src/test/java/com/codinggame/training/ChuckNorrisEncoderTest.java
@@ -0,0 +1,76 @@
+package com.codinggame.training;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codinggame.training.ChuckNorrisEncoder.charEncode;
+import static com.codinggame.training.ChuckNorrisEncoder.encode;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+
+class ChuckNorrisEncoderTest
+{
+    @Test
+    void shouldReturnNullIfProvidedWithNull() {
+        assertNull(encode(null));
+    }
+
+    @Test
+    void shouldReturnEmptyStringIfProvidedWithEmptyString() {
+        assertThat(encode("")).isEqualTo("");
+    }
+
+    @Test
+    void shouldEncode_A()
+    {
+        // A 01000001
+        assertThat(encode("A")).isEqualTo("0 0 00 00000 0 0");
+    }
+
+    @Test
+    void shouldEncode_B()
+    {
+        // B 01000010
+        assertThat(encode("B")).isEqualTo("0 0 00 0000 0 0 00 0");
+    }
+
+    @Test
+    void shouldEncode_C()
+    {
+        // C 01000011
+        assertThat(encode("C")).isEqualTo("0 0 00 0000 0 00");
+    }
+
+    @Test
+    @Tag("single_char")
+    void shouldCharEncode_0()
+    {
+        // '\u0000
+        assertThat(charEncode('\u0000')).isEqualTo("00 0");
+    }
+
+    @Test
+    @Tag("single_char")
+    void shouldCharEncode_1()
+    {
+        // '\u0001
+        assertThat(charEncode('\u0001')).isEqualTo("0 0");
+    }
+
+    @Test
+    @Tag("single_char")
+    void shouldCharEncode_10()
+    {
+        // '\u0002
+        assertThat(charEncode('\u0002')).isEqualTo("0 0 00 0");
+    }
+
+    @Test
+    @Tag("single_char")
+    void shouldCharEncode_11()
+    {
+        // '\u0003
+        assertThat(charEncode('\u0003')).isEqualTo("0 00");
+    }
+}
